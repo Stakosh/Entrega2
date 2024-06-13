a098247a5c7e45d80179428828043cf3d2465492
Stakosh <soto.javi1@gmail.com>
2024-06-13 15:26:40 -0400

    arreglo v3

--- services/backend/entrypoint.sh
+++ services/backend/entrypoint.sh
@@ -12,6 +12,15 @@ while ! nc -z db 5432; do
   echo "PostgreSQL no disponible, reintentando en 0.3 segundos..."
   sleep 0.3
 done
+
+# Verificar que PostgreSQL esté listo para aceptar conexiones
+echo "---------------"
+echo "Verificando que PostgreSQL esté listo para aceptar conexiones..."
+until pg_isready -h db -p 5432 -U postgres; do
+  echo "PostgreSQL no está listo, reintentando en 0.3 segundos..."
+  sleep 0.3
+done
+echo "PostgreSQL está listo para aceptar conexiones en el puerto 5432"
 echo "---------------"
 
 echo "═══•◉•═════
@@ -40,15 +49,18 @@ echo "═══•◉•═════
 
 ╬═╬/ \ "
 
-# Ejecutar el script Python
+# Ejecutar el script Python en segundo plano
 echo "---------------"
 echo "Ejecutando main.py con los argumentos 'run -h 0.0.0.0'"
 echo "---------------"
-python main.py
+python main.py &
 
 # Mensaje final para indicar que el script ha terminado
 echo "---------------"
 echo "Script de inicio completado"
 echo ":)"
 echo "---------------"
-echo " MAY THE FORCE BE WITH YOU"
+echo "MAY THE FORCE BE WITH YOU"
+
+# Mantener el contenedor en ejecución
+wait
